d9394126dfe557cb3391d3b3158d2b5c4e3a9f08
Gabriel Sales Rodrigues <88689778+gaabpng@users.noreply.github.com>
2025-07-18 19:32:18 -0300

    fix: correção do dashboard com simulação de valores

--- Web/Telemetria/monitoramento/urls.py
+++ Web/Telemetria/monitoramento/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import LoraCreateView, LoraListView, dashboard
+from .views import LoraCreateView, LoraListView, LoraLastView, dashboard
 
 urlpatterns = [
     path('dados/', LoraCreateView.as_view(), name='criar-lora'),
     path('dados/lista/', LoraListView.as_view(), name='listar_lora'),
-    path('dashboard/', dashboard, name='dashboard')
+    path('dashboard/', dashboard, name='dashboard'),
+    path('dados/ultimo/', LoraLastView.as_view(), name='lora-ultimo')
+
 ]
--- Web/Telemetria/monitoramento/views.py
+++ Web/Telemetria/monitoramento/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import Lora
 from .serializers import LoraSerializer
 
@@ -11,5 +13,11 @@ class LoraListView(generics.ListAPIView):
     queryset = Lora.objects.order_by('-timestamp')[:50]
     serializer_class = LoraSerializer
 
+class LoraLastView(APIView):
+    def get(self, request):
+        ultimo = Lora.objects.order_by('-timestamp', '-id').first()
+        serializer = LoraSerializer(ultimo)
+        return Response(serializer.data)
+    
 def dashboard(request):
     return render(request, 'monitoramento/dashboard.html')
